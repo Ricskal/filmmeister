ce2fc5b2d8d0f0892f79b5705b6b32c2809f9ec0
Rick <rickensinkrullen@gmail.com>
2021-10-02 21:20:41 +0200

    Pagina  titel

--- app/routes.py
+++ app/routes.py
@@ -3,4 +3,12 @@ from app import app_inst
 @app_inst.route('/')
 @app_inst.route('/index')
 def index():
-    return "Berend Joris Jan Rick"
+    return '''
+    <html>
+        <head>
+            <title>Der Filmmeister Hauptseite</title>
+        </head>
+        <body>
+            Berend, Joris, Jan, Rick
+        </body>
+    </html>'''
