5904416a2364a0b9f0470631fa0d95d0841da8d0
Rick <rickensinkrullen@gmail.com>
2021-10-06 09:31:57 +0200

    Inlog form toegevoegd

--- app/forms.py
+++ app/forms.py
@@ -4,3 +4,6 @@ from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember me')
+    submit = SubmitField('Sign in')
--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,17 @@
 from flask import render_template
 from app import app_inst
+from app.forms import LoginForm
+
 
 @app_inst.route('/')
 @app_inst.route('/index')
 def index():
     title = 'Hauptseite'
     body = 'Brend, Joris, Jan, Rick'
-    return render_template('index.html', title = title, body = body)
+    return render_template('index.html', title=title, body=body)
+
+
+@app_inst.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login/html', title='Sign in', form=form)
