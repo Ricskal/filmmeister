9271ffbd54aa0b06c64e54d2644ac80f0f5b7125
Rick <rickensinkrullen@gmail.com>
2021-10-07 18:52:16 +0200

    Begin Database

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app_inst = Flask(__name__)
 app_inst.config.from_object(Config)
+db = SQLAlchemy(app_inst)
+migrate = Migrate(app_inst, db)
 
-from app import routes
+from app import routes, models
 
 
 # source /Home/filmmeister/venv/bin/activate
