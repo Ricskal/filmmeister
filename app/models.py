55829e61bd2a3f8f1cd9fe683169442efe54527a
Rick <rickensinkrullen@gmail.com>
2021-10-07 20:09:18 +0200

    Error in user table

--- app/models.py
+++ app/models.py
@@ -7,7 +7,6 @@ class User(db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    movie =
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
